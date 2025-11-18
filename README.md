538f539214a51d9a1605da6f5e9a1ab6fb59cc2a
sanketh <sankethsd@gmail.com>
2025-11-18 22:54:38 +0530

    addnew line readmi and new file created

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 this is my first repository
 <br>
 and my name is  sanketh
+added another line
+
---
+++ newfl.sh
@@ -0,0 +1,9 @@
+add new line as demo for devops
+echo "This is a new line added as a demo for DevOps."
+echo "DevOps demo line added successfully." 
+echo "End of DevOps demo script."
+echo "This line signifies the completion of the DevOps demo script."
+echo "Thank you for using the DevOps demo script."
+echo "Goodbye from the DevOps demo script!"
+echo "This is the final line of the DevOps demo script."
+echo "DevOps demo script has ended."     
